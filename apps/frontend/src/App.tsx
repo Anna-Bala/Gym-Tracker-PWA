a9d0665215c0b3a1589a316bf3804b6be398334f
Anna-Bala <annabala.contact@gmail.com>
2025-08-10 15:22:32 +0200

    dist: Install and setup react-router-dom

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import Layout from "./components/base/Layout";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="registration" element={<Registration />} />
+        <Route path="*" element={<p>Page not found!</p>} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
