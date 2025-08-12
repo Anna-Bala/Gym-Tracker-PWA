d9042f71351b67066fb9956d3b9255638c0a25f7
Anna-Bala <annabala.contact@gmail.com>
2025-08-12 21:39:07 +0200

    minor: Improve SignupSchema error messages

--- apps/schemas/src/users.ts
+++ apps/schemas/src/users.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const SignupSchema = z
   .object({
-    firstName: z.string().trim().min(2, { message: "FirstName must be at least 2 characters long" }).max(50, { message: "FirstName must not exceed 50 characters" }),
-    lastName: z.string().trim().min(2, { message: "LastName must be at least 2 characters long" }).max(50, { message: "LastName must not exceed 50 characters" }),
+    firstName: z.string().trim().min(2, { message: "First name must be at least 2 characters long" }).max(50, { message: "First name must not exceed 50 characters" }),
+    lastName: z.string().trim().min(2, { message: "Last name must be at least 2 characters long" }).max(50, { message: "Last name must not exceed 50 characters" }),
     email: z.email({ message: "Invalid email address" }),
     confirmPassword: z.string(),
     password: z
