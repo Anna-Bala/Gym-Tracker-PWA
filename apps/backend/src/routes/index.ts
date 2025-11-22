3c5b6ff3df97d636c314a002e004a23b06adb5cc
Anna-Bala <annabala.contact@gmail.com>
2025-11-22 11:43:34 +0100

    feat: Add new endpoint for getting user statistics

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import authRoutes from "./auth";
-import onboardingRoutes from "./onboarding";
 import exercisesRoutes from "./exercises";
+import onboardingRoutes from "./onboarding";
+import userRoutes from "./user";
 
 const rootRouter: Router = Router();
 
 rootRouter.use("/auth", authRoutes);
 rootRouter.use("/onboarding", onboardingRoutes);
 rootRouter.use("/exercises", exercisesRoutes);
+rootRouter.use("/user", userRoutes);
 
 export default rootRouter;
