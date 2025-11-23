07ade233cbf4106f24ceaa086b2cf3c2b1dbdd74
Anna-Bala <annabala.contact@gmail.com>
2025-11-23 17:54:08 +0100

    feat: Add Profile and Security settings layout

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -19,6 +19,7 @@ import History from "./pages/history/History";
 import Layout from "./components/base/Layout";
 import Login from "./pages/auth/Login";
 import PrivateRoute from "./components/base/PrivateRoute";
+import ProfileSettings from "./pages/settings/ProfileSettings";
 import Registration from "./pages/auth/Registration";
 import Report from "./pages/report/Report";
 import Settings from "./pages/settings/Settings";
@@ -43,6 +44,7 @@ function App() {
             <Route path="/report" element={<Report />} />
             <Route path="/history" element={<History />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/settings/profile" element={<ProfileSettings />} />
             <Route path="/onboarding/1" element={<OnboardingStepGender />} />
             <Route path="/onboarding/2" element={<OnboardingStepFocusArea />} />
             <Route path="/onboarding/3" element={<OnboardingStepAge />} />
