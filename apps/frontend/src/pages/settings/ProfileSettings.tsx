85f7bc108b90e74b01fc60b56223a4ba7739f6bf
Anna-Bala <annabala.contact@gmail.com>
2025-11-23 17:54:08 +0100

    feat: Add Profile and Security settings layout

---
+++ apps/frontend/src/pages/settings/ProfileSettings.tsx
@@ -0,0 +1,119 @@
+import { Link } from "react-router-dom";
+import { useActionState } from "react";
+import { useForm } from "react-hook-form";
+import { z, ProfileSettingsSchema } from "@gym-tracker-pwa/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button, Input } from "@/components/ui";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Typography } from "@/components/base/Typography";
+import Bin from "@icons/bin.svg?react";
+import Chevron from "@icons/chevron.svg?react";
+import Locked from "@icons/locked.svg?react";
+
+type ProfileSettingsFormData = z.infer<typeof ProfileSettingsSchema>;
+
+const ProfileSettings = () => {
+  const form = useForm<ProfileSettingsFormData>({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    resolver: zodResolver(ProfileSettingsSchema),
+  });
+
+  const {
+    formState: { errors },
+  } = form;
+
+  const [state, submitAction, isPending] = useActionState(() => {}, { success: null });
+
+  return (
+    <section>
+      <Typography className="w-full text-center font-semibold" variant="h2">
+        Profile & Security
+      </Typography>
+
+      <Typography className="w-full font-semibold mt-6" variant="h4">
+        Personal Info
+      </Typography>
+
+      <Form {...form}>
+        <form action={submitAction} noValidate>
+          <div className="w-full flex flex-col gap-4 mt-4">
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  {errors.firstName && <FormMessage>{errors.firstName.message}</FormMessage>}
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  {errors.lastName && <FormMessage>{errors.lastName.message}</FormMessage>}
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" {...field} />
+                  </FormControl>
+                  {errors.email && <FormMessage>{errors.email.message}</FormMessage>}
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex w-full fixed bottom-0 left-0 px-6 py-4 bg-background border-t border-muted shadow-wide-xl">
+            <Button className="w-full" type="submit">
+              Save
+            </Button>
+          </div>
+        </form>
+      </Form>
+
+      <Typography className="w-full font-semibold mt-6" variant="h4">
+        Account security
+      </Typography>
+
+      <Link className="text-base flex items-center !px-0 justify-start text-foreground py-2 gap-4 font-medium mt-1" to="/settings/profile/password">
+        <>
+          <Locked className="!w-10 !h-10 text-foreground" />
+          <Typography variant="md-24">Change password</Typography>
+          <Chevron className="!w-6 !h-6 rotate-180 ml-auto mr-2" />
+        </>
+      </Link>
+
+      <Button className="w-full text-base !p-0 text-destructive justify-center gap-4 mt-3" variant="link" size="lg" onClick={() => {}}>
+        <>
+          <Bin className="!w-10 !h-10 text-destructive" />
+          Delete account
+          <Chevron className="!w-6 !h-6 rotate-180 ml-auto mr-2" />
+        </>
+      </Button>
+      <Typography className="px-2 text-muted-foreground" variant="sm-20">
+        Permanently remove your account and data. Proceed with caution.
+      </Typography>
+    </section>
+  );
+};
+
+export default ProfileSettings;
--- apps/schemas/src/index.ts
+++ apps/schemas/src/index.ts
@@ -1,5 +1,6 @@
 export * from "zod";
 export * from "./exercises";
 export * from "./onboarding";
+export * from "./settings";
 export * from "./statistics";
 export * from "./users";
