db9b53c694fc854fb9b819f1d8e8fd97f93f51c6
Anna-Bala <annabala.contact@gmail.com>
2025-11-30 00:51:44 +0100

    feat: Add new PATCH endpoint for editing user body metrics data

--- apps/backend/src/controllers/onboarding.ts
+++ apps/backend/src/controllers/onboarding.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { FullOnboardingSchema } from "@gym-tracker-pwa/schemas";
+import { BodyMetricsSchema, FullOnboardingSchema } from "@gym-tracker-pwa/schemas";
 import { BadRequestException } from "../exceptions/bad-request";
 import { ErrorCode } from "../exceptions";
 import { prismaClient } from "..";
@@ -30,4 +30,39 @@ export const create = async (req: Request, res: Response) => {
   });
 
   res.json(onboarding);
+};
+
+export const patch = async (req: Request, res: Response) => {
+  const userId = req.userId;
+
+  const { age, gender, height, weight } = BodyMetricsSchema.parse(req.body);
+
+  let onboarding = await prismaClient.onboarding.findFirst({ where: { userId } });
+  if (!onboarding) {
+    throw new BadRequestException("Onboarding data is missing", ErrorCode.USER_ONBOARDING_MISSING);
+  }
+
+  const {
+    age: newAge,
+    gender: newGender,
+    height: newHeight,
+    weight: newWeight,
+  } = await prismaClient.onboarding.update({
+    where: {
+      id: onboarding.id,
+    },
+    data: {
+      age,
+      gender,
+      height,
+      weight,
+    },
+  });
+
+  return res.status(200).json({
+    age: newAge,
+    gender: newGender,
+    height: newHeight,
+    weight: newWeight,
+  });
 };
