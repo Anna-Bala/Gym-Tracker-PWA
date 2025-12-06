a6de94850b662696c2c7301fed9efebae3a1f428
Anna-Bala <annabala.contact@gmail.com>
2025-12-07 00:08:42 +0100

    feat: Add endpoint for changing user password

--- apps/backend/src/routes/user.ts
+++ apps/backend/src/routes/user.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 
 import { authenticateToken } from "../middlewares/authenticateToken";
 import { errorHandler } from "../error-handler";
-import { get, getUserStatistics, patch } from "../controllers/user";
+import { changePassword, get, getUserStatistics, patch } from "../controllers/user";
 
 const userRouter: Router = Router();
 
 userRouter.get("/", authenticateToken, errorHandler(get));
 userRouter.patch("/", authenticateToken, errorHandler(patch));
+userRouter.patch("/password", authenticateToken, errorHandler(changePassword));
 userRouter.get("/statistic", authenticateToken, errorHandler(getUserStatistics));
 
 export default userRouter;
