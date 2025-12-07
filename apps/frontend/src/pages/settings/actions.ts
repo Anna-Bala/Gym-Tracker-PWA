d27755db945c83163e2c91a8d2b42de60d325220
Anna-Bala <annabala.contact@gmail.com>
2025-12-07 21:20:56 +0100

    feat: Make it possible to save personal info changes in settings tab

--- apps/frontend/src/pages/settings/actions.ts
+++ apps/frontend/src/pages/settings/actions.ts
@@ -1,8 +1,30 @@
 import { authFetch } from "@/lib/fetchClient";
 
-export interface ChangePasswordActionState {
+interface HandleProfileSettingSaveState {
   success: boolean | null;
 }
+
+interface ChangePasswordActionState {
+  success: boolean | null;
+}
+
+export const handleProfileSettingSave = async (_prevState: HandleProfileSettingSaveState, data: FormData): Promise<HandleProfileSettingSaveState> => {
+  try {
+    const formData = Object.fromEntries(data);
+    const response = await authFetch("/user", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(formData),
+    });
+
+    if (!response.ok) return { success: false };
+
+    return { success: true };
+  } catch {
+    return { success: false };
+  }
+};
 
 export const handleChangePasswordAction = async (_prevState: ChangePasswordActionState, data: FormData): Promise<ChangePasswordActionState> => {
   try {
