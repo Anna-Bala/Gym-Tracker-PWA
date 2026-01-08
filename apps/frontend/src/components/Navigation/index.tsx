bcf9d6fc1bb4b16d1c9f06ba36446a0be766dbee
Anna-Bala <annabala.contact@gmail.com>
2026-01-08 13:31:30 +0100

    feat: Make navigation desktop friendly

--- apps/frontend/src/components/Navigation/index.tsx
+++ apps/frontend/src/components/Navigation/index.tsx
@@ -5,22 +5,25 @@ import { cn } from "@/lib/utils";
 import { navigationItems } from "./constants";
 import { Typography } from "@/components/base/Typography";
 
-interface BottomNavigation {
+interface Navigation {
   isHidden?: boolean;
   children: ReactNode;
 }
 
-export const BottomNavigation: React.FC<BottomNavigation> = ({ children, isHidden }) => {
+export const Navigation: React.FC<Navigation> = ({ children, isHidden }) => {
   if (isHidden) return children;
 
   return (
     <>
       {children}
-      <div className="flex justify-between w-full fixed bottom-0 left-0 px-6 py-4 bg-background border-t border-muted shadow-wide-xl gap-4 z-20">
+      <div className="flex justify-between w-full fixed bottom-0 left-0 px-6 py-4 bg-background border-t border-muted shadow-wide-xl gap-4 z-20 lg:w-1/12 lg:sticky lg:top-8 lg:border-0 lg:shadow-none lg:flex-col lg:max-h-[90vh] lg:gap-12 lg:justify-start">
         {navigationItems.map(({ customIconClasses, Icon, label, location }) => (
           <NavLink
             className={({ isActive }) =>
-              cn("flex flex-1 flex-col items-center", { "text-accent-foreground font-bold stroke-[3]": isActive, "stroke-2 text-muted-foreground dark:text-accent": !isActive })
+              cn("flex flex-1 flex-col items-center lg:justify-center lg:flex-none lg:last:mt-auto", {
+                "text-accent-foreground font-bold stroke-[3]": isActive,
+                "stroke-2 text-muted-foreground dark:text-accent": !isActive,
+              })
             }
             to={location}
             key={location}
@@ -36,4 +39,4 @@ export const BottomNavigation: React.FC<BottomNavigation> = ({ children, isHidde
   );
 };
 
-BottomNavigation.displayName = "BottomNavigation";
+Navigation.displayName = "Navigation";
