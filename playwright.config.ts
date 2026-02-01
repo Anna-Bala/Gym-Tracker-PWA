a2ff6cc61bf9b2f9bcc4e1666f9a1e598f1a2662
Anna-Bala <annabala.contact@gmail.com>
2026-02-01 19:17:03 +0100

    fix: Set headless parameter to true for CI and fix baseUrl

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,11 +17,11 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: "http://localhost:5173",
+    baseURL: process.env.CI ? "http://127.0.0.1:5173" : "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    headless: false,
+    headless: !!process.env.CI,
   },
 
   /* Configure projects for major browsers */
@@ -34,8 +34,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm run dev",
-    url: "http://localhost:5173",
+    command: process.env.CI ? "pnpm -C apps/frontend run dev --host 127.0.0.1" : "pnpm run dev",
+    url: process.env.CI ? "http://127.0.0.1:5173" : "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
   },
 });
