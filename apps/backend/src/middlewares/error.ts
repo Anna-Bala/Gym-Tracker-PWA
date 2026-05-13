a79027396eb77680b2c0aaef7ccabbac97ebc152
Anna-Bala <annabala.contact@gmail.com>
2026-05-13 09:26:26 +0200

    fix: Make it easier to read server errors

--- apps/backend/src/middlewares/error.ts
+++ apps/backend/src/middlewares/error.ts
@@ -2,5 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import { HttpException } from "../exceptions";
 
 export const errorMiddleware = (error: HttpException, _req: Request, response: Response, _next: NextFunction) => {
+  console.error(error);
   response.status(error.statusCode).json({ message: error.message, errorCode: error.errorCode, errors: error.error });
 };
